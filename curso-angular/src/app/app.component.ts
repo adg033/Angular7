9993c3fa9ef58de53556308d3c39d427f07821cc
adg033 <acidg0@hotmail.com>
2018-11-07 13:20:52 +0100

    hoy

---
+++ curso-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iam-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'curso-angular';
+  logo = '../assets/logo.svg';
+}
---
+++ curso-angular/src/app/app.module.ts
@@ -0,0 +1,19 @@
+// Para que lo sepa EM6
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Para que lo sepa angular
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
