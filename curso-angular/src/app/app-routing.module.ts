c8e87cfe30aefa5f84ca54fcaec74786a5fd08bf
adg033 <acidg0@hotmail.com>
2018-11-12 10:34:41 +0100

    enrutamiento

--- curso-angular/src/app/app-routing.module.ts
+++ curso-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'home', component: HomeComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- curso-angular/src/app/tasks/tasks.module.ts
+++ curso-angular/src/app/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TasksComponent],
   imports: [
     CommonModule,
     TasksRoutingModule
